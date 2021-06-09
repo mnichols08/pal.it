935ede7f60500ca7177282c3e07f411139504a33
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2021-06-09 11:35:40 -0400

    created copy button

--- src/color-box.js
+++ src/color-box.js
@@ -4,10 +4,16 @@ import './ColorBox.css'
 
 class ColorBox extends Component {
     render() {
+        const { name, background } = this.props
         return (
-            <div style={{ background: this.props.background }} className='ColorBox'>
-                <span>{ this.props.name }</span>
-                <span>MORE</span>
+            <div style={{ background }} className='ColorBox'>
+                <div className='copy-container'>
+                    <div className='box-content'>
+                        <span>{ name }</span>
+                    </div>
+                </div>
+                <button className='copy-button'>Copy</button>
+                <span>More</span>
             </div>
         )
     }
