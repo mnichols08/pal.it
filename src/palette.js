a421731e6ceefe21121cf21236b652890d2ac5ac
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2021-06-09 10:18:32 -0400

    added palette.js

---
+++ src/palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className='Palette'>
+                { /*  NAVBAR  */}
+                <div className='Palette-colors'>
+                    { /*  COLOR BOXES  */}
+                </div>
+                { /*  FOOTER  */}
+            </div>
+        )
+    }
+}
+
+export default Palette
