0565c24e97846aa2b9a5cf1dc18c894cc89c5d3a
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2021-06-09 15:57:59 -0400

    Added withStyles and demo styles for mini-palette

--- src/components/palette-list/index.jsx
+++ src/components/palette-list/index.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import MiniPalette from '../mini-palette/'
+
 class PaletteList extends Component {
     render() {
         const { palettes } = this.props
         return (
             <div>
+                <MiniPalette />
                 <h1>pal.it</h1>
                 {palettes.map(palette => (
                     <p>
