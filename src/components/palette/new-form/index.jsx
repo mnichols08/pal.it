e18cf707aa2495ed2825b5ff4b046bdaab4e1193
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2021-06-10 00:25:23 -0400

    added chromepicker and buttons to drawer

--- src/components/palette/new-form/index.jsx
+++ src/components/palette/new-form/index.jsx
@@ -10,6 +10,8 @@ import Divider from'@material-ui/core/Divider'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import Button from '@material-ui/core/Button'
+import { ChromePicker } from 'react-color'
 
 import styles from './styles'
 
@@ -37,7 +39,7 @@ class NewPaletteForm extends Component {
                             <MenuIcon />
                         </IconButton>
                         <Typography variant='h6' color='inherit' noWrap>
-                            Persistent drawer
+                            Persistent Drawer
                         </Typography>
                     </Toolbar>
                 </AppBar>
@@ -56,6 +58,22 @@ class NewPaletteForm extends Component {
                         </IconButton>
                     </div>
                     <Divider />
+                    <Typography variant='h4'>Design Your pal.it</Typography>
+                    <div>
+                        <Button variant='contained' color='secondary'>
+                            Clear Palette
+                        </Button>
+                        <Button variant='contained' color='primary'>
+                            Random Color
+                        </Button>
+                    </div>
+                    <ChromePicker
+                        color='purple'
+                        onChangeComplete={newColor => console.log(newColor)}
+                    />
+                    <Button variant='contained' color='primary'>
+                        Add Color
+                    </Button>
                 </Drawer>
                 <main className={classNames(classes.content, {
                     [classes.contentShift]: open
